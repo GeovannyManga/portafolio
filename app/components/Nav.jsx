54e734c3b1055a880a52464fca33c59de6863e3b
GeovannyManga <geovannyx.casimirom@unac.edu.co>
2025-04-27 15:31:21 -0500

    terminada parcial de el nav

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -2,8 +2,18 @@ import React from 'react'
 
 export default function Nav() {
   return (
-    <div>
-        <h1>Hola soy una nav</h1>
+    <div className='flex items-center justify-between bg-slate-950 h-20 w-full px-8'>
+        <div className=''>
+            <span className='font-montserrat text-4xl font-bold text-gray-800 text-white'>Geo</span>
+            <span className='font-montserrat text-4xl font-black text-purple-700'>Dev</span>
+        </div>
+        <ul className=' font-montserrat flex text-white items-center justify-evenly w-1/2 h-auto  font-medium'>
+            <li>Sobre mi</li>
+            <li>Servicios</li>
+            <li>Trabajos</li>
+            <li>Consulta</li>
+            <li className='h-12 w-32 bg-black flex justify-center items-center bg-purple-900 rounded-lg'>Contáctame</li>
+        </ul>
     </div>
   )
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,11 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Montserrat } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+// Aquí configuramos la fuente Montserrat
+const montserrat = Montserrat({
+  variable: "--font-montserrat",  // Variable CSS para usarla globalmente
+  subsets: ["latin"],  // Subconjuntos de caracteres, puedes agregar más si es necesario
 });
 
 export const metadata: Metadata = {
@@ -24,9 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${montserrat.variable} antialiased`}>
         {children}
       </body>
     </html>
